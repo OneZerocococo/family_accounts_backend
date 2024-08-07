c6a0312d7802f84059516d69f38de3c73b18a886
OneZerocococo <ylflyfly@gmail.com>
2024-08-07 20:27:08 +0800

    feat: add create a transaction api

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const transactionController = require('./controllers/transaction-controller')
 app.use(express.json())
 app.get('/transactions/:group_id', transactionController.getTransactions)
 app.delete('/transaction/:id', transactionController.removeOne)
+app.post('/transaction', transactionController.createTransaction)
 
 app.listen(process.env.PORT, () => {
   console.log('App is running!')
--- controllers/transaction-controller.js
+++ controllers/transaction-controller.js
@@ -2,10 +2,49 @@ const client = require('../config/mongodb')
 const db = client.db()
 const Transaction = db.collection('transactions')
 
+const generateRandomId = () => {
+  return Math.random().toString(36).substr(2, 12).toUpperCase(); // 生成 12 位隨機 ID
+};
+
 const transactionController = {
+  createTransaction: async (req, res) => {
+    try {
+      const { date, group_id, img, category_id, amount, description } = req.body;
+
+      if (!date || !group_id || !img || !category_id || !amount || !description) {
+        return res.status(400).json({ result: 'failed', message: 'Missing required fields' });
+      }
+
+      const parsedDate = new Date(date);
+      if (isNaN(parsedDate.getTime())) {
+        return res.status(400).json({ result: 'failed', message: 'Invalid date format' });
+      }
+
+      const id = generateRandomId();
+
+      const newTransaction = {
+        date: parsedDate,
+        group_id,
+        img,
+        category_id,
+        amount,
+        id,
+        description,
+      };
+
+      const result = await Transaction.insertOne(newTransaction);
+      if (result.acknowledged) {
+        const insertedDoc = await Transaction.findOne({ _id: result.insertedId });
+        return res.status(201).json({ result: 'success', data: insertedDoc });
+      }
+      return res.status(500).json({ result: 'failed', message: 'Failed to insert document' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ result: 'error', message: error.message });
+    }
+  },
   getTransactions: async (req, res) => {
     try {
-      // 預設撈當月
       const now = new Date()
       const startOfMonth = new Date(Date.UTC(now.getFullYear(), now.getMonth(), 1, -8, 0, 0))
       const endOfMonth = new Date(Date.UTC(now.getFullYear(), now.getMonth() + 1, 1, -8, 0, 0))
