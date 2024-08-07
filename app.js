97f69c16194bd36387103c275c7afef8196dcb34
OneZerocococo <ylflyfly@gmail.com>
2024-08-08 01:43:15 +0800

    add cors setting

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const cors = require('cors')
 const express = require('express')
 const app = express()
+
 const transactionController = require('./controllers/transaction-controller')
+
+app.use(cors())
 app.use(express.json())
 app.get('/transactions/:group_id', transactionController.getTransactions)
 app.delete('/transaction/:id', transactionController.removeOne)
