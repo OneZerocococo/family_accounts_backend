11d5853546303f45fbe55f94474bad90a19bbc95
OneZerocococo <ylflyfly@gmail.com>
2024-08-19 14:31:15 +0800

    modify: get categories api response remove _id

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -3,7 +3,7 @@ const Category = require('../models/category')
 const categoryController = {
   getCategories: async (req, res, next) => {
     try {
-      const categories = await Category.find().lean()
+      const categories = await Category.find().select('-_id').lean()
       res.status(200).json(categories)
     } catch (error) {
       next(error)
